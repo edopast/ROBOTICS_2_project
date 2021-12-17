9aff508b2a0eb0d5a641e79ecb23a27db441f42e
Edoardo Pastorello <edoardo.pastorello@gmail.com>
2021-12-17 16:35:57 +0100

    AGV control

---
+++ AGV_ctrl_params.m
@@ -0,0 +1,13 @@
+%% UGV control parameters
+
+ctrl.k_roll_p = 2.44;
+ctrl.k_roll_d = 0.568;
+ctrl.k_roll_N = 1000;
+
+ctrl.k_pitch_p = 2.44;
+ctrl.k_pitch_d = 0.568;
+ctrl.k_pitch_N = 1000;
+
+ctrl.k_yaw_p = 1.1577;
+ctrl.k_yaw_d = 0.53899;
+ctrl.k_yaw_N = 1000;
---
+++ AGV_trajectory_generation.m
@@ -0,0 +1,29 @@
+%% AGV trajectory generation
+% always start from [0 0 0]
+
+% Experimental time [s]
+T = 20;
+Ts = 1e-3;
+
+%% desired position trajectory
+
+p_ref.signals.values = zeros(T/Ts, 3);  % [ x , y , z ]
+p_ref.signals.dimensions = 3;
+p_ref.time = zeros(T/Ts,1);
+
+yaw_ref.signals.values = zeros(T/Ts, 1);  % [ \psi ]
+yaw_ref.signals.dimensions = 1;
+yaw_ref.time = zeros(T/Ts,1);
+
+for i = 1:(T/Ts-1)
+    p_ref.signals.values(i+1, 3) = 1.2; % z
+    p_ref.signals.values(i+1, 1) = cos(0.0314*i);    % each millisecond: 1.8 degrees in x and y
+    p_ref.signals.values(i+1, 2) = sin(0.0314*i);
+    yaw_ref.signals.values(i+1, 1) = 0;
+    p_ref.time(i+1) = Ts*i;
+    yaw_ref.time(i+1) = Ts*i;
+end
+
+plot3(p_ref.signals.values(:,1), p_ref.signals.values(:,2), p_ref.signals.values(:,3))
+figure(2)
+plot(yaw_ref.time, yaw_ref.signals.values)
