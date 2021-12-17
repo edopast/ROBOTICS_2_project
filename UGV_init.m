44d480abaac209cf10c5f9b3044e9c89e828aaad
Gioele Ambrosin <gioele.ambrosin@studenti.unipd.it>
2021-12-17 19:40:42 +0100

    clean files

---
+++ UGV_init.m
@@ -0,0 +1,10 @@
+
+% Initialize the Unicycle model
+run initializeUnicycleModel.m
+
+% Generate the trajectory
+run UGV_trajectory_generation.m
+
+% Set the controller parameters
+run UGV_ctrl_params.m
+
