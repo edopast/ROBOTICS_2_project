57e8edee4ba1786b100a66e291f5acb71140e415
andreolijacopo98 <pino310198@gmail.com>
2021-12-17 14:24:13 +0100

    traiettoria UGV impostata su circolare; guadagni dei controlli impostati a seconda della traiettoria richiesta.

--- UGV_ctrl_params.m
+++ UGV_ctrl_params.m
@@ -1,5 +1,8 @@
 %% UGV control parameters
+ctrl.ksi = 0.01;
+ctrl.a = 5;
+%considering linear/circular trajectory
 
-ctrl.k1 = 10;
-ctrl.k2 = 5;
-ctrl.k3 = 10;
+ctrl.k1 = 2*ctrl.ksi*ctrl.a;
+ctrl.k2 = (((ctrl.a^2)*constant_desired_velocity_w)/constant_desired_velocity_v);
+ctrl.k3 = ctrl.k1;
