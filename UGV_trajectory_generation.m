fa51b3c64aaafdbccdb3941f0239fe44a9c79c4a
Edoardo Pastorello <edoardo.pastorello@gmail.com>
2021-12-17 09:51:51 +0100

    minor fix in UGV_trajectory_generation

--- UGV_trajectory_generation.m
+++ UGV_trajectory_generation.m
@@ -5,24 +5,6 @@
 T = 20;
 Ts = 1e-3;
 
-% %% x, y trajectories and theta angles to track
-% trajectory.signals.values = zeros(T/Ts,2);
-% trajectory.signals.dimensions = 2;
-% trajectory.time = zeros(T/Ts,1);
-% 
-% heading.signals.values = zeros(T/Ts,1);
-% heading.signals.dimensions = 1;
-% heading.time = zeros(T/Ts,1);
-% m = 0.5;
-% 
-% for i = 0:(T/Ts-1)
-%     trajectory.signals.values(i+1,:) = [Ts*i, m*Ts*i];
-%     heading.signals.values(i+1,:) = atan(m);
-%     trajectory.time(i+1) = Ts*i;
-% end
-% 
-% heading.time = trajectory.time;
-
 %% linear and angular desired velocities
 w_ref.signals.values = zeros(T/Ts,1);
 w_ref.signals.dimensions = 1;
