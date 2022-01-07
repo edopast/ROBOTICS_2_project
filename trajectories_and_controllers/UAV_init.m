49745b0e2b7043a08631878dd7713bc1dff99afa
Edoardo Pastorello <edoardo.pastorello@gmail.com>
2022-01-07 15:37:17 +0100

    UAV_init completed.

--- trajectories_and_controllers/UAV_init.m
+++ trajectories_and_controllers/UAV_init.m
@@ -1,9 +1,13 @@
 clear all
 close all
+
+% Generate the trajectory
+run UGV_init.m
+close all
+
 % Initialize the Unicycle model
 run initializeQuadrotorModel.m
 
-% Generate the trajectory
 run UAV_trajectory_generation.m
 
 % Set the controller parameters
@@ -17,17 +21,26 @@ UAVRealPose = sim('UAV_2021_a.slx', 'ReturnWorkspaceOutputs', 'on');
 %% plot desired trajectory
 %hold off
 figure(2)
-%hold on
 plot3(p_ref.signals.values(:,1), p_ref.signals.values(:,2), p_ref.signals.values(:,3), 'b', 'LineWidth', 1);
+hold on
 grid on;
 title('UAVtrajectory');
 legendInfo=string(zeros(2,1));
 legendInfo(1, 1) = sprintf('Desired');
-
-% plot real trajectory
-plot(AGVRealPose.Pose.signals.values(:,1), AGVRealPose.Pose.signals.values(:,2), 'r-.', 'LineWidth', 1);
 legendInfo(2, 1) = sprintf('True');
 
-legend(legendInfo(:,:),'Location','SouthEast');
-xlabel('x --> [m]')
-ylabel('y --> [m]')
+% plot real trajectory
+plot3(UAVRealPose.Pose.signals.values(:,1), UAVRealPose.Pose.signals.values(:,2), UAVRealPose.Pose.signals.values(:,3), 'r-.', 'LineWidth', 1);
+
+% and also yaw angle (occasionally)
+for j=1:400:(size(UAVRealPose.Pose.signals.values,1))
+    UAV_xd = UAVRealPose.Pose.signals.values(j,1) + 0.3*cos(UAVRealPose.Pose.signals.values(j,6));
+    UAV_yd = UAVRealPose.Pose.signals.values(j,2) + 0.3*sin(UAVRealPose.Pose.signals.values(j,6));
+    plot3([UAVRealPose.Pose.signals.values(j,1) UAV_xd], [UAVRealPose.Pose.signals.values(j,2) UAV_yd], [UAVRealPose.Pose.signals.values(j,3) UAVRealPose.Pose.signals.values(j,3)], 'g', 'LineWidth', 1.5);
+end
+
+legend(legendInfo(1:2,:),'Location','SouthEast');
+
+xlabel('x [m]')
+ylabel('y [m]')
+zlabel('z [m]')
--- trajectories_and_controllers/UAV_trajectory_generation.m
+++ trajectories_and_controllers/UAV_trajectory_generation.m
@@ -15,23 +15,35 @@ yaw_ref.signals.values = zeros(T/Ts, 1);  % [ \psi ]
 yaw_ref.signals.dimensions = 1;
 yaw_ref.time = zeros(T/Ts,1);
 
-for i = 1:(T/Ts-1)
+for i = 1:(T/Ts-1) %take off (1.5 seconds, then let z be just 1 m)
     p_ref.signals.values(i+1, 3) = 1; % z
-    p_ref.signals.values(i+1, 1) = 1;
-    p_ref.signals.values(i+1, 2) = 0;
-    yaw_ref.signals.values(i+1, 1) = 0;
+    p_ref.signals.values(i+1, 1) = 0; % x
+    p_ref.signals.values(i+1, 2) = 0; % y
     p_ref.time(i+1) = Ts*i;
     yaw_ref.time(i+1) = Ts*i;
 end
 
-for i = round((T/Ts)/3):(T/Ts-1)
-    p_ref.signals.values(i+1, 1) = 1*cos(0.0005*(i-round((T/Ts)/3)));    % each millisecond: 1.8 degrees in x and y
-    p_ref.signals.values(i+1, 2) = 1*sin(0.0005*(i-round((T/Ts)/3)));
-    %yaw_ref.signals.values(i+1, 1) = 0.0005*(i-((T/Ts)/2));
-    p_ref.time(i+1) = Ts*i;
-    yaw_ref.time(i+1) = Ts*i;
+
+for i = 2:(T/Ts-1)
+    % try to increment the number of points (UGVRealPose has 100 points a
+    % sec, while we generate 1000 points a sec, with a convex combination)
+    j = floor(i*0.1);
+    prevj = j-1;
+    if prevj<1
+        p_ref.signals.values(i+1, 1) = p_ref.signals.values(i, 1);
+        p_ref.signals.values(i+1, 2) = p_ref.signals.values(i, 2);
+        yaw_ref.signals.values(i+1, 1) = yaw_ref.signals.values(i, 1);
+        continue;
+    end
+    %p_ref.signals.values(i+1, 1) = ugv.trajectory.signals.values(i,1);
+    %p_ref.signals.values(i+1, 2) = ugv.trajectory.signals.values(i,1);
+    p_ref.signals.values(i+1, 1) = (1-i*0.1+j)*UGVRealPose.Pose.signals.values(prevj,1) + (i*0.1-j)*UGVRealPose.Pose.signals.values(j,1);
+    p_ref.signals.values(i+1, 2) = (1-i*0.1+j)*UGVRealPose.Pose.signals.values(prevj,2) + (i*0.1-j)*UGVRealPose.Pose.signals.values(j,2);
+    
+    % yaw angle
+    yaw_ref.signals.values(i+1,1) = (UGVRealPose.Pose.signals.values(j,3));
 end
 
 %plot3(p_ref.signals.values(:,1), p_ref.signals.values(:,2), p_ref.signals.values(:,3))
-%figure(2)
+%figure(3)
 %plot(yaw_ref.time, yaw_ref.signals.values)
